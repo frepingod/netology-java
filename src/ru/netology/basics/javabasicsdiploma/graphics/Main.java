938497da151c11a0c222b10b4b00253d9839def1
frepingod <frepingod@gmail.com>
2021-06-19 16:17:02 +0300

    refactor

--- src/ru/netology/basics/javabasicsdiploma/graphics/Main.java
+++ src/ru/netology/basics/javabasicsdiploma/graphics/Main.java
@@ -2,7 +2,6 @@ package ru.netology.basics.javabasicsdiploma.graphics;
 
 import ru.netology.basics.javabasicsdiploma.graphics.image.TextGraphicsConverter;
 import ru.netology.basics.javabasicsdiploma.graphics.image.TextGraphicsConverterImpl;
-import ru.netology.basics.javabasicsdiploma.graphics.server.GServer;
 
 import java.io.File;
 import java.io.PrintWriter;
@@ -11,17 +10,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         TextGraphicsConverter converter = new TextGraphicsConverterImpl();
 
-        //GServer server = new GServer(converter); // Создаём объект сервера
-        //server.start(); // Запускаем
-
-        // Или то же, но с сохранением в файл:
-        ///*
         PrintWriter fileWriter = new PrintWriter(new File("src/ru/netology/basics/javabasicsdiploma/converted-image.txt"));
         converter.setMaxWidth(200);
         converter.setMaxHeight(300);
-        //fileWriter.write(converter.convert("https://i.ibb.co/6DYM05G/edu0.jpg")); //example
         fileWriter.write(converter.convert("https://proprikol.ru/wp-content/uploads/2020/10/kartinki-ne-grusti-1.jpg")); //example
         fileWriter.close();
-        //*/
     }
 }
