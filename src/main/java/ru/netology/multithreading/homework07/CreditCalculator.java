a15642c1525d31607b60cea42bca0fbd36d22321
frepingod <frepingod@gmail.com>
2021-09-11 20:28:04 +0300

    hw7.1 multithreading - tests

---
+++ src/main/java/ru/netology/multithreading/homework07/CreditCalculator.java
@@ -0,0 +1,16 @@
+package ru.netology.multithreading.homework07;
+
+public class CreditCalculator {
+
+    public int monthlyPayment(int creditAmount, int termInMonths, double creditRate) {
+        return 0;
+    }
+
+    public int totalAmount(int creditAmount, int termInMonths, double creditRate) {
+        return 0;
+    }
+
+    public int overpayment(int creditAmount, int termInMonths, double creditRate) {
+        return 0;
+    }
+}
