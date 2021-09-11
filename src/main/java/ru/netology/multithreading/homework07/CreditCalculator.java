b9544f45aa1c8c449a5dd667239001ea7f54935f
frepingod <frepingod@gmail.com>
2021-09-11 20:28:51 +0300

    hw7.1 multithreading - code

--- src/main/java/ru/netology/multithreading/homework07/CreditCalculator.java
+++ src/main/java/ru/netology/multithreading/homework07/CreditCalculator.java
@@ -3,14 +3,25 @@ package ru.netology.multithreading.homework07;
 public class CreditCalculator {
 
     public int monthlyPayment(int creditAmount, int termInMonths, double creditRate) {
-        return 0;
+        double monthlyInterestRate = monthlyInterestRate(creditRate);
+        double annuityCoefficient = annuityCoefficient(termInMonths, monthlyInterestRate);
+        return (int) Math.round(creditAmount * annuityCoefficient);
     }
 
     public int totalAmount(int creditAmount, int termInMonths, double creditRate) {
-        return 0;
+        return monthlyPayment(creditAmount, termInMonths, creditRate) * termInMonths;
     }
 
     public int overpayment(int creditAmount, int termInMonths, double creditRate) {
-        return 0;
+        return totalAmount(creditAmount, termInMonths, creditRate) - creditAmount;
+    }
+
+    protected double monthlyInterestRate(double creditRate) {
+        return creditRate / 100 / 12;
+    }
+
+    protected double annuityCoefficient(int termInMonths, double monthlyInterestRate) {
+        double pow = Math.pow(1 + monthlyInterestRate, termInMonths);
+        return (monthlyInterestRate * pow) / (pow - 1);
     }
 }
